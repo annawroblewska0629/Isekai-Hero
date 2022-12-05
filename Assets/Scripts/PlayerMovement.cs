3e5eae59a738ca44e7a64a9930059756ed35a28f
annawroblewska0629 <61706728+annawroblewska0629@users.noreply.github.com>
2022-12-05 13:46:37 +0100

    Added pushing obstacle logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,45 +7,63 @@ using UnityEngine.Tilemaps;
 public class PlayerMovement : MonoBehaviour
 {
     private float deathDealy = .5f;
+    PlayerControls playerControls;
+    Vector3 directionVector3;
 
-    [Header("MovmentScore")]
-    [SerializeField] int startMovmentScore;
-    
-    [Header("Tilemap")] 
+    [Header("MovementLimit")]
+    [SerializeField] int startMovementLimit;
+
+    [Header("Tilemap")]
     [SerializeField] Tilemap groundTilemap;
     [SerializeField] Tilemap obstacleTilemap;
+    [SerializeField] TileBase obstacleTile;
     [SerializeField] int cellSize = 1;
 
     private void Awake()
     {
-        PlayerControls playerControls = new PlayerControls();
+        playerControls = new PlayerControls();
         playerControls.Player.Enable();
         playerControls.Player.Move.performed += Move;
 
-       
+
     }
+   
     private void Update()
     {
         if (isDead())
         {
             StartCoroutine(Die());
+            playerControls.Player.Move.Disable();
         }
+        
     }
 
     private void Move(InputAction.CallbackContext context)
     {
-        Vector2 direction = context.ReadValue<Vector2>() * cellSize;
-        if (CanMove(direction))
+        Vector2 directionVector2 = context.ReadValue<Vector2>() * cellSize;
+        directionVector3 = (Vector3)directionVector2;
+        if (CanMove(directionVector3))
         {
-            transform.position += (Vector3)direction;
-            startMovmentScore--;
+            transform.Translate(directionVector3);
+            startMovementLimit--;
         }
+        if(CanPush(directionVector3))
+          {
+            Vector3Int targetGridPositionToMove = groundTilemap.WorldToCell(transform.position + directionVector3);
+            Vector3Int targetGridPositionToPush = groundTilemap.WorldToCell(transform.position + directionVector3 + directionVector3);
+            
+            obstacleTilemap.SetTile(targetGridPositionToPush, obstacleTile);
+            obstacleTilemap.SetTile(targetGridPositionToMove, null);
+            transform.Translate(directionVector3);
+            startMovementLimit--;
+          }
+        
     }
 
-    private bool CanMove(Vector2 direction)
+    private bool CanMove(Vector3 direction)
    { 
-        Vector3Int targetGridPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
-        if(!groundTilemap.HasTile(targetGridPosition) || obstacleTilemap.HasTile(targetGridPosition) || isDead())
+        Vector3Int targetGridPositionToMove = groundTilemap.WorldToCell(transform.position + direction);
+        if(!groundTilemap.HasTile(targetGridPositionToMove) || obstacleTilemap.HasTile(targetGridPositionToMove))
         {
             return false;
         }
@@ -54,10 +72,26 @@ public class PlayerMovement : MonoBehaviour
             return true;
         }
     }
+    private bool CanPush(Vector3 direction)
+    {
+        Vector3Int targetGridPositionToMove = obstacleTilemap.WorldToCell(transform.position + direction);
+        Vector3Int targetGridPositionToPush = obstacleTilemap.WorldToCell(transform.position + directionVector3 + directionVector3);
+        //Vector3Int targetGridPositionToPush = targetGridPositionToMove + groundTilemap.WorldToCell(direction);
+        if ( obstacleTilemap.HasTile(targetGridPositionToMove) && !obstacleTilemap.HasTile(targetGridPositionToPush) && groundTilemap.HasTile(targetGridPositionToPush) )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 
+    }
+   
+   
     private bool isDead()
     {
-        if (startMovmentScore == 0)
+        if (startMovementLimit == 0)
         {
             return true;
         }
@@ -75,6 +109,6 @@ public class PlayerMovement : MonoBehaviour
 
     public int GetMovmentScore()
     {
-        return startMovmentScore;
+        return startMovementLimit;
     }
 }
